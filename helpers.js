60ee40ff1ebf32837beb2e53d40cd72916d6f67c
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-19 00:29:45 -0400

    fixed checkUserOwnUrl so urlObj checks its userID property

--- helpers.js
+++ helpers.js
@@ -9,20 +9,20 @@ function generateShortURL(length) {
 function checkUserID(users, id) {
   return Object.keys(users).indexOf(id) > -1;
 }
-function getUserByEmail(users, email) {
+function getUserByEmail(email, users) {
   for (let key of Object.keys(users)) {
     let userObj = users[key];
     if (userObj.email === email) {
       return userObj;
     }
   }
-  return null;
+  return undefined;
 }
 function loggedIn(users, userID) {
   return userID && checkUserID(users, userID);
 }
 
-function filteredUrlDatabase (urlDatabase, userID) {
+function filteredUrlDatabase(urlDatabase, userID) {
   const filtered = {};
   for (let key of Object.keys(urlDatabase)) {
     if (urlDatabase[key].userID === userID) {
@@ -39,4 +39,11 @@ function checkUserOwnUrl(urlObj, userID) {
   return false;
 }
 
-module.exports = { generateShortURL, getUserByEmail, checkUserID, loggedIn, filteredUrlDatabase, checkUserOwnUrl};
+module.exports = {
+  generateShortURL,
+  getUserByEmail,
+  checkUserID,
+  loggedIn,
+  filteredUrlDatabase,
+  checkUserOwnUrl,
+};
