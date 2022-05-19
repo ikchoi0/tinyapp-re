725b4b340ba36347b1ff274b3c608dc5f0a3408c
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-19 11:57:10 -0400

    split auth function into 2 functions

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,5 +13,22 @@ function auth(req, res, next) {
   }
   next();
 }
+function checkLogIn(req, res, next){
+  req.userID = req.session.user_id || {};
+  const user = users[req.userID] || {};
+  if (!loggedIn(users, req.userID)) {
+    return res.status(403).render('error', {user, error: "You need to log in first!"});
+  }
+  next();
+}
+function checkPermission(req, res, next) {
+  req.userID = req.session.user_id || {};
+  req.shortURL = req.params.shortURL;
+  const user = users[req.userID] || {};
+  if (!checkUserOwnUrl(urlDatabase[req.shortURL], req.userID)) {
+    return res.status(403).render('error', {user, error: "You do not have permission!"});
+  }
+  next();
+}
 
 module.exports = { auth };
